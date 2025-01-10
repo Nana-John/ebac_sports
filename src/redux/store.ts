d8339d701be958282504e447d97da57b939949cd
Nana-John <alessandramairis@gmail.com>
2025-01-10 10:55:22 -0300

    Adicionado slice do carrinho com Redux Toolkit

---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from './apiSlice'
+import cartSlice from './cartSlice'
+
+const store = configureStore({
+  reducer: {
+    cart: cartSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware)
+})
+
+export default store
